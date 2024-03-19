22b5857f478e4c664a834ca7bce0f438d0ab14bf
madara-tribe <tsukuba.univer@gmail.com>
2024-03-19 12:01:59 +0900

    update

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 from PySide6.QtGui import Qt
 from PySide6.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QDockWidget
 from qtWidgets.RightCamWidget import RightCamWidget
-from qtWidgets.LeftWidget import LeftWidget
 
 class MyMainWindow(QMainWindow):
     def __init__(self, opt, parent=None):
@@ -15,24 +14,10 @@ class MyMainWindow(QMainWindow):
         # RIGHT Side camera widget
         here_path = os.path.dirname(os.path.abspath(__file__))
         self.plot_layout = QVBoxLayout()
-        self.right_video_path = os.path.join(here_path, opt.right_video_path)
         self.right_widget = RightCamWidget(self, opt)
         self.plot_layout.addWidget(self.right_widget)
         self.setCentralWidget(self.right_widget)
     
-        
-        # Left side widget
-        cap = self.right_widget.return_cap()
-        self.leftDock = QDockWidget("Left Widget", self)
-        self.leftside = LeftWidget(self, cap)
-        self.leftDock.setWidget(self.leftside)
- 
-        self.leftDock.setAllowedAreas(Qt.LeftDockWidgetArea
-                                   | Qt.RightDockWidgetArea)
-        self.leftDock.setFeatures(QDockWidget.DockWidgetMovable
-                                  | QDockWidget.DockWidgetFloatable)
-        #self.addDockWidget(Qt.LeftDockWidgetArea, self.leftDock)
-        
         
 def main(opt):
     app = QApplication(sys.argv)
@@ -48,10 +33,10 @@ if __name__ == '__main__':
     parser.add_argument('--yolov7_onnx_path', type=str, default='weights/yolov7Tiny_640_640.onnx', help='image path')
     parser.add_argument('--midas_onnx_path', type=str, default='weights/model-f6b98070.onnx', help='onnx midas weight model')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='conf threshold for NMS or postprocess')
-    parser.add_argument('--right_video_path', type=str, default='data/outdriving.mov', help='right video path')
+    parser.add_argument('--video_path', type=str, default='data/outdriving.mov', help='right video path')
     parser.add_argument('-o', '--output_file', type=str, default='data/movie', help='movie output path')
-    parser.add_argument('--height', type=int, default=500, help='height of movie')
-    parser.add_argument('--width', type=int, default=1200, help='width of of movie')
+    parser.add_argument('--height', type=int, default=400, help='height of movie')
+    parser.add_argument('--width', type=int, default=800, help='width of of movie')
     #parser.add_argument('-s', '--show', action='store_true', help='prepare test data')
     opt = parser.parse_args()
     try:
--- qtWidgets/RightCamWidget.py
+++ qtWidgets/RightCamWidget.py
@@ -1,7 +1,9 @@
 import cv2
+import time
+import numpy as np
 from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout
-from PySide6.QtCore import QSize
-
+from PySide6.QtCore import QSize, QTimer
+from PySide6.QtGui import QImage, QPixmap
 #from onnx_inference import image_main, movie_main
 
 
@@ -33,6 +35,86 @@ class RightUI(QWidget):
         self.predictbar.setStyleSheet('font-family: Times New Roman; font-size: 15px; color: black; background-color: azure')
         self.predictor_layout.addWidget(self.predictor_title)
         self.predictor_layout.addWidget(self.predictbar)
+
+        
+class RightCamWidget(QWidget):
+    def __init__(self, parent, opt):
+        super().__init__(parent=parent)
+        
+        self.parent = parent
+        self.right_capture = cv2.VideoCapture(opt.video_path)
+        
+        self.W, self.H = opt.width, opt.height
+        self.video_size = QSize(self.W, self.H)
+        self.TIMEOUT = 1
+        self.cur_fps = 0
+        self.old_timestamp = time.time()
+        self.setup_ui()
+        self.setup_camera()
+        self.plot_fps(initial=True)
+        
+    def setup_ui(self):
+        """Initialize widgets.
+        """
+        self.set1_calcurate_bar_layout()
+        self.set2_video_bar_layout()
+        self.set3_main_layout()
+        
+    def set1_calcurate_bar_layout(self):
+        # Predicted time bar
+        self.predictor_layout = QHBoxLayout()
+        self.predictor_title = QLabel('Predicted time')
+        self.predictbar = QLabel('', self)
+        self.predictbar.setStyleSheet('font-family: Times New Roman; font-size: 15px; color: black; background-color: azure')
+        self.predictor_layout.addWidget(self.predictor_title)
+        self.predictor_layout.addWidget(self.predictbar)
+
+        # FPS bar
+        self.fps_layout = QHBoxLayout()
+        self.fps_title = QLabel('Constant FPS')
+        self.fps = QLabel('', self)
+        self.fps.setStyleSheet('font-family: Times New Roman; font-size: 15px; color: black; background-color: azure')
+        self.fps_layout.addWidget(self.fps_title)
+        self.fps_layout.addWidget(self.fps)
+        
+    def set2_video_bar_layout(self):
+        # video widget
+        self.video_widget = QLabel()
+        self.video_widget.setFixedSize(self.video_size)
+        
+    def set3_main_layout(self):
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addWidget(self.video_widget)
+        self.main_layout.addLayout(self.predictor_layout)
+        self.main_layout.addLayout(self.fps_layout)
+        self.setLayout(self.main_layout)
+        
+    def setup_camera(self):
+        """Initialize camera.
+        """
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.display_video_stream)
+        self.timer.start(30)
+
+    def display_video_stream(self):
+        """Read frame from camera and repaint QLabel widget.
+        """
+        _, rframe = self.right_capture.read()
+        fps = (time.time() - self.old_timestamp) / self.TIMEOUT
+        if (time.time() - self.old_timestamp) > self.TIMEOUT:
+            start = time.time()
+            # right prediction
+            right_pred_frame = rframe  #movie_main(self.onnx_path, rframe.copy())
+            self.predict_time = np.round((time.time() - start), decimals=5)
+            pred_frame = cv2.resize(right_pred_frame, (self.W, self.H))
+            
+            image = QImage(pred_frame, pred_frame.shape[1], pred_frame.shape[0],
+                            pred_frame.strides[0], QImage.Format_RGB888)
+            self.video_widget.setPixmap(QPixmap.fromImage(image))
+            self.old_timestamp = time.time()
+            
+            self.cur_fps = np.round(fps, decimals=3)
+            self.plot_fps()
         
     def plot_fps(self, initial=None):
         if initial:
@@ -40,90 +122,3 @@ class RightUI(QWidget):
         else:
             self.fps.setText(str(self.cur_fps))
             self.predictbar.setText(str(self.predict_time*1000)+"[ms]")
-
-
-        
-        
-class RightCamWidget(QWidget):
-    def __init__(self, parent, opt):
-        super().__init__(parent=parent)
-        
-        self.parent = parent
-        self.RUI = RightUI(self, opt)
-        self.cap, out_frame, frame_width, frame_height = self.get_cap(opt)
-        
-        # video wiget
-        #self.size = 500
-        #self.video_size = QSize(self.size*2, self.size)
-        #self.video_widget = QLabel()
-        #self.video_widget.setFixedSize(self.video_size)
-        
-        
-        #self.TIMEOUT = 1
-        #self.cur_fps = 0
-        #self.onnx_path = onnx_path
-        #self.old_timestamp = time.time()
-        self.setup_ui(opt)
-        #self.setup_camera()
-        
-    def setup_ui(self, opt):
-        self.main_layout = QVBoxLayout()
-        self.setLayout(self.main_layout)
-        self.RUI.plot_fps(initial=True)
-        self.main_layout.addWidget(self.RUI.video_widget)
-        self.main_layout.addLayout(self.RUI.predictor_layout)
-        self.main_layout.addLayout(self.RUI.fps_layout)
-
-    def get_cap(self, opt):
-        cap = cv2.VideoCapture(opt.right_video_path)
-        if not cap.isOpened():
-            print('can not open video')
-            exit()
-        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = float(cap.get(cv2.CAP_PROP_FPS))  # FPS
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out_frame = cv2.VideoWriter(opt.output_file+"_onnx.mp4", fourcc, fps, (frame_width, frame_height), isColor=False)
-        return cap, out_frame, frame_width, frame_height
-        
-    """
-    def setup_ui(self):
-        
-        self.set1_calcurate_bar_layout()
-        self.set2_video_bar_layout()
-    
-        
-        
-    #def set3_main_layout(self):
-        
-        #self.main_layout.addWidget(self.video_widget)
-    
-    def setup_camera(self):
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.display_video_stream)
-        self.timer.start(30)
-
-    def display_video_stream(self):
-        
-        _, rframe = self.right_capture.read()
-        fps = (time.time() - self.old_timestamp) / self.TIMEOUT
-        if (time.time() - self.old_timestamp) > self.TIMEOUT:
-            start = time.time()
-            # right prediction
-            right_pred_frame = movie_main(self.onnx_path, rframe.copy())
-            self.predict_time = np.round((time.time() - start), decimals=5)
-            pred_frame = cv2.resize(right_pred_frame, (self.size, self.size*2))
-            
-            image = QImage(pred_frame, pred_frame.shape[0], pred_frame.shape[1],
-                            pred_frame.strides[0], QImage.Format_RGB888)
-            self.video_widget.setPixmap(QPixmap.fromImage(image))
-            self.old_timestamp = time.time()
-            
-            self.cur_fps = np.round(fps, decimals=3)
-            self.plot_fps()
-            
-    """
-    def return_cap(self):
-        return self.cap
-        
-    
